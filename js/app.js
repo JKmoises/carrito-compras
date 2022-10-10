e0e8ff32b34fb63f1148a2015df861f6643bf9d7
JKmoises <moi.prado@duocuc.cl>
2022-10-10 19:43:56 -0300

    Renderizado de productos

--- js/app.js
+++ js/app.js
@@ -1,5 +1,52 @@
 document.addEventListener('DOMContentLoaded', iniciarApp);
 
+let productos = [
+  {
+    nombre: "Funko Pop",
+    precio: 25000,
+    unidades: 5
+  },
+  {
+    nombre: "Harry Potter DVD",
+    precio: 3500,
+    unidades: 50
+  },
+  {
+    nombre: "Mouse Gamer Pro",
+    precio: 130000,
+    unidades: 80
+  },
+  {
+    nombre: "Macbook Air Apple",
+    precio: 900000,
+    unidades: 3
+  },
+];
+
+let carrito = [];
+
 function iniciarApp(){
+  renderizarProductos();
+}
+
+function renderizarProductos() {
+  const $productosContainer = document.querySelector('#productos');
+  const $template = document.querySelector('#template-producto').content;
+  const $fragment = document.createDocumentFragment();
+
+  productos.forEach(({nombre,precio,unidades}) => {
+    $template.querySelector('.nombre').textContent = nombre;
+    $template.querySelector('.unidades').innerHTML = `<sub>${unidades}</sub> unidades`;
+    $template.querySelector('.precio').innerHTML = `<sup>$</sup>${precio}`;
+    $template.querySelector('.btn-producto').onclick = agregarEnCarrito;
+
+    let clone = document.importNode($template,true);
+
+    $fragment.appendChild(clone);
+  });
+  $productosContainer.appendChild($fragment);
+}
+
+function agregarEnCarrito(){
   
 }
