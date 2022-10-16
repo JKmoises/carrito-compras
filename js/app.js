e09b6bd5822cec585615099c55e00fc6cee38994
JKmoises <moi.prado@duocuc.cl>
2022-10-16 16:59:20 -0300

    Calculo de total del carrito completado

--- js/app.js
+++ js/app.js
@@ -1,112 +1,146 @@
-document.addEventListener('DOMContentLoaded', iniciarApp);
+document.addEventListener("DOMContentLoaded", iniciarApp);
 
 let productos = [
   {
     id: 1,
     nombre: "Funko Pop",
-    imagen: 'funkopop.jpg',
+    imagen: "funkopop.jpg",
     precio: 25000,
-    unidades: 5
+    unidades: 5,
   },
   {
     id: 2,
     nombre: "Harry Potter DVD",
-    imagen: 'harrypotter.jpg',
+    imagen: "harrypotter.jpg",
     precio: 3500,
-    unidades: 50
+    unidades: 50,
   },
   {
     id: 3,
     nombre: "Mouse Gamer Pro",
-    imagen: 'mouse.jpg',
+    imagen: "mouse.jpg",
     precio: 130000,
-    unidades: 80
+    unidades: 80,
   },
   {
     id: 4,
     nombre: "Macbook Air Apple",
-    imagen: 'macbook.jpg',
+    imagen: "macbook.png",
     precio: 900000,
-    unidades: 3
+    unidades: 3,
+  },
+  {
+    id: 5,
+    nombre: "Libro Clean Javascript",
+    imagen: "librojs.jpg",
+    precio: 50000,
+    unidades: 10,
   },
 ];
 
 let objProductoCarrito = {
-  id: '',
-  nombre: '',
+  id: "",
+  nombre: "",
   precio: 0,
-  unidades: 0,
-  total: 0
-}
+  subtotal: 0,
+  unidades: 1,
+  total: 0,
+};
 
 let carrito = [];
 
-
-function iniciarApp(){
+function iniciarApp() {
   renderizarProductos();
   animarCardProducto();
   agregarProductoEnCarrito();
+  calcularTotalCompra();
 }
 
-
 function renderizarProductos() {
-  const $productosContainer = document.querySelector('#productos');
-  const $template = document.querySelector('#template-producto').content;
+  const $productosContainer = document.querySelector("#productos");
+  const $template = document.querySelector("#template-producto").content;
   const $fragment = document.createDocumentFragment();
 
-  productos.forEach(({ id, nombre,imagen, precio, unidades }) => {
-    $template.querySelector('.producto').dataset.productoId = id;
-    $template.querySelector('.producto-img > img').src = `assets/${imagen}`;
-    $template.querySelector('.nombre').textContent = nombre;
-    $template.querySelector('.unidades').innerHTML = /* html */`<sub>${unidades}</sub> unidades`;
-    $template.querySelector('.precio').innerHTML = /* html */`<sup>$</sup>${formatPrecio(precio)}`;
-    
-    let clone = document.importNode($template,true);
-    
+  productos.forEach(({ id, nombre, imagen, precio, unidades }) => {
+    $template.querySelector(".producto").dataset.productoId = id;
+    $template.querySelector(".producto-img > img").src = `assets/${imagen}`;
+    $template.querySelector(".producto-img > img").alt = nombre;
+    $template.querySelector(".nombre").textContent = nombre;
+    $template.querySelector(
+      ".unidades"
+    ).innerHTML = /* html */ `<sub>${unidades}</sub> unidades`;
+    $template.querySelector(
+      ".precio"
+    ).innerHTML = /* html */ `<sup>$</sup>${formatValor(precio)}`;
+
+    let clone = document.importNode($template, true);
+
     $fragment.appendChild(clone);
   });
   $productosContainer.appendChild($fragment);
 }
 
 function agregarProductoEnCarrito() {
-  const $botonesProductos = document.querySelectorAll('.btn-producto');
+  const $botonesProductos = document.querySelectorAll(".btn-producto");
 
-  $botonesProductos.forEach($btnProducto => {
-    $btnProducto.addEventListener('click', e => {
+  $botonesProductos.forEach(($btnProducto) => {
+    $btnProducto.addEventListener("click", (e) => {
       let productoId = parseInt(e.target.parentElement.dataset.productoId);
-      let producto = productos.filter(producto => producto.id === productoId);
-      let [{ id, nombre, precio, unidades }] = producto;
-      
+      let producto = productos.filter((producto) => producto.id === productoId);
+      let [{ id, nombre, precio }] = producto;
+
       objProductoCarrito.id = id;
       objProductoCarrito.nombre = nombre;
       objProductoCarrito.precio = precio;
-      objProductoCarrito.unidades = unidades;
+      objProductoCarrito.subtotal = precio;
+
+      let existeProducto = carrito.find(
+        (producto) => producto.id === objProductoCarrito.id
+      );
+      if (existeProducto) return;
 
       calcularCompra();
 
-      carrito = [...carrito, {...objProductoCarrito}];
+      carrito = [...carrito, { ...objProductoCarrito }];
+      // console.log(carrito);
       renderizarCarrito();
     });
   });
 }
 
-function calcularCompra(){
-  
+function calcularCompra() {
+  calcularTotalCompra();
+}
+
+function calcularTotalCompra() {
+  const $totalCarrito = document.querySelector(".total");
+
+  objProductoCarrito.total += objProductoCarrito.precio;
+
+  let totalCarrito =
+    objProductoCarrito.total === 0
+      ? objProductoCarrito.total
+      : formatValor(objProductoCarrito.total);
+
+  $totalCarrito.textContent = `$${totalCarrito}`;
 }
 
 function renderizarCarrito() {
   limpiarHTML();
 
-  const $carritoContainer = document.querySelector('#carrito');
-  const $template = document.querySelector('#template-carrito').content;
+  const $carritoContainer = document.querySelector("#carrito");
+  const $template = document.querySelector("#template-carrito").content;
   const $fragment = document.createDocumentFragment();
 
-  carrito.forEach(({ id,nombre, precio, unidades },index) => {
-    $template.querySelector('.producto-carrito').dataset.productoId = id;
-    $template.querySelector('.nombre').textContent = nombre;
-    $template.querySelector('.nombre').classList.add('text-center');
-    $template.querySelector('.unidades').innerHTML = unidades;
-    $template.querySelector('.precio').innerHTML = `$${formatPrecio(precio)}`;
+  carrito.forEach(({ id, nombre, precio, subtotal, unidades }) => {
+    $template.querySelector(".producto-carrito").dataset.productoId = id;
+    $template.querySelector(".nombre").textContent = nombre;
+    $template.querySelector(".nombre").classList.add("text-center");
+    $template.querySelector(".unidades").innerHTML = unidades;
+    $template.querySelector(".precio").innerHTML = `$${formatValor(precio)}`;
+    $template.querySelector(".subtotal").innerHTML = `$${formatValor(
+      subtotal
+    )}`;
 
     let clone = document.importNode($template, true);
 
@@ -115,31 +149,31 @@ function renderizarCarrito() {
   $carritoContainer.appendChild($fragment);
 }
 
-function formatPrecio(precio) {
+function formatValor(precio) {
   const options = {
-    style: 'currency',
-    currency: 'CLP'
-  }
+    style: "currency",
+    currency: "CLP",
+  };
 
-  return precio.toLocaleString('es-ES',options);
+  return precio.toLocaleString("es-ES", options);
 }
 
 function animarCardProducto() {
-  const $botonesProductos = document.querySelectorAll('.btn-producto');
+  const $botonesProductos = document.querySelectorAll(".btn-producto");
 
-  $botonesProductos.forEach(($btnProducto,i) => {
-    $btnProducto.addEventListener('mouseenter', () => {
-      $btnProducto.parentElement.classList.add('animar-producto')
+  $botonesProductos.forEach(($btnProducto, i) => {
+    $btnProducto.addEventListener("mouseenter", () => {
+      $btnProducto.parentElement.classList.add("animar-producto");
     });
 
-    $btnProducto.addEventListener('mouseout', () => {
-      $btnProducto.parentElement.classList.remove('animar-producto')
+    $btnProducto.addEventListener("mouseout", () => {
+      $btnProducto.parentElement.classList.remove("animar-producto");
     });
   });
 }
 
-function limpiarHTML(){
-  const $carrito = document.querySelector('.carrito');
+function limpiarHTML() {
+  const $carrito = document.querySelector(".carrito");
 
   while ($carrito.firstElementChild) {
     $carrito.firstElementChild.remove();
